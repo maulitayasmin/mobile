99b73c6824d8405926c73df90198a4462c78c36a
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-08-14 19:55:40 +0700

    center hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //harus diinmport dulu
 
 void main() {
   runApp(MyApp()); //fungsi untuk menjalankan aplikasi
                   //MyApp adalah nama sebuah class dari app yang akan dijalankan
+                  // runApp bisa dijalankan kalau udah mengimport
 }
 
 //stl -> membuat otomatis kode dibawah ini
@@ -10,9 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( //pada lukisan diibaratkan seperti kayu
       home: Scaffold(   //Wadah untuk mengoding
-        body: Text('Hello World'),  //widget Text('...') digunakan untuk mengisi tulisan
+        // backgroundColor: Colors.green[900],
+        // backgroundColor: Color(0xFFD51212), // penulisan background yang lain
+        body: Center( // center digunakan agar tulisan ke tengah
+          child: Text('Hello World') //widget Text('...') digunakan untuk mengisi tulisan
+          ),
       ), 
     );
   }
