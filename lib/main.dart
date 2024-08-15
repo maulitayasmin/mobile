6b779cf388c9d5ffee1d8e6bbea0363655582104
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-08-15 22:52:17 +0700

    image

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,18 @@ class LitaApp extends StatelessWidget {
           centerTitle: true, //karena di android centerTitle nya false jadi harus di setting dulu menjadi true
         ),
         body: Center(
-          child: Icon(
-            Icons.home, //digunakan untuk menentukan icon apa yang akan digunakan
-            size: 200, //digunakan untuk mengatur ukuran icon
-            color: Colors.green, //digunakan untuk memberikan warna icon
-          )
+          // Image Provider ada 4 :
+          // 1. Asset Image -> gambar yang ada dalam folder froject
+          // child: Image(
+          //   image: AssetImage('img/joe.jpg'), //harus didaftarkan di pubspec.yaml dulu
+          // )
+          // 2. Network Image -> gambar yang diambil di internet
+          // child: Image( //aplikasi butuh koneksi internet
+          //   image: NetworkImage('https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1634025439/01hxjxy42rf1kp87qatraerd4w.jpg')
+          // )
+          // Jarang digunakan
+          // 3. File Image -> 
+          // 4. Memory Image ->
         )
       )
     );
