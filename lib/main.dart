97c49d394a3ab94b71a571f06c67e7879c102b3f
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-08-15 19:50:10 +0700

    appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart'; //harus diinmport dulu
+import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp()); //fungsi untuk menjalankan aplikasi
-                  //MyApp adalah nama sebuah class dari app yang akan dijalankan
-                  // runApp bisa dijalankan kalau udah mengimport
+void main(){
+  runApp(LitaApp());
 }
 
-//stl -> membuat otomatis kode dibawah ini
-class MyApp extends StatelessWidget {
+class LitaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( //pada lukisan diibaratkan seperti kayu
-      home: Scaffold(   //Wadah untuk mengoding
-        // backgroundColor: Colors.green[900],
-        // backgroundColor: Color(0xFFD51212), // penulisan background yang lain
-        body: Center( // center digunakan agar tulisan ke tengah
-          child: Text('Hello World') //widget Text('...') digunakan untuk mengisi tulisan
-          ),
-      ), 
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar( //mengedit pada bagian atas
+          title: Text('Aplikasi Lita'),
+          backgroundColor: const Color.fromARGB(255, 80, 159, 82),
+          centerTitle: true, //karena di android centerTitle nya false jadi harus di setting dulu menjadi true
+        ),
+        body: Center(
+          child: Text('Hello World:)'),
+        )
+      )
     );
   }
 }
