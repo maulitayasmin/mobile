270b55f580f4a39c62580df05def1dce69156b42
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-08-20 20:46:23 +0700

    listview.builder

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,6 @@ void main(){
 class LitaApp extends StatelessWidget {
   LitaApp({super.key});
 
-  //Berupa Widget
-//   List<KotakWarna> allitems = List.generate(10,
-//    (index) => KotakWarna(
-//     nama: 'Kotak ke - ${index + 1}', 
-//     warna: Color.fromARGB(
-//       255, 
-//       100 + Random().nextInt(256), 
-//       100 + Random().nextInt(256), 
-//       100 + Random().nextInt(256)
-//       ),
-//     ),
-// );
-
-  List<Map<String, dynamic>> data = List.generate(
-    10, 
-    (index) => {
-      'nama': 'Kotak ke - ${index + 1}',
-      'warna': Color.fromARGB(
-      255, 
-      100 + Random().nextInt(256), 
-      100 + Random().nextInt(256), 
-      100 + Random().nextInt(256)
-      ),
-    }
-  );
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,65 +19,18 @@ class LitaApp extends StatelessWidget {
           backgroundColor: Colors.blue,
           foregroundColor: Colors.white,
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: 
-              data.map(
-                (e) => KotakWarna(
-                  nama: e['nama'], 
-                  warna: e['warna'],
-                  ),
-                  )
-                  .toList(), //harus dikasih toList()
-              // KotakWarna(nama: 'Merah', warna: Colors.red,),
-              // KotakWarna(nama: 'Kuning', warna: Colors.amber,),
-              // KotakWarna(nama: 'Hijau', warna: Colors.green,),
-              // KotakWarna(nama: 'Abu-abu', warna: Colors.grey,),
-              // KotakWarna(nama: 'Pink', warna: Colors.pink,),
-
-              // Container(
-              //   width: 200,
-              //   height: 200,
-              //   color: Colors.amber,
-              //   child: Center(
-              //     child: Text('Kuning')
-              //   ),
-              // ),
-              // Container(
-              //   width: 200,
-              //   height: 200,
-              //   color: Colors.green,
-              //   child: Center(
-              //     child: Text('Hijau')
-              //   ),
-              // ),
-              // Container(
-              //   width: 200,
-              //   height: 200,
-              //   color: Colors.red,
-              //   child: Center(
-              //     child: Text('Merah')
-              //   ),
-              // ),
-              // Container(
-              //   width: 200,
-              //   height: 200,
-              //   color: Colors.amber,
-              //   child: Center(
-              //     child: Text('Kuning')
-              //   ),
-              // ),
-              // Container(
-              //   width: 200,
-              //   height: 200,
-              //   color: Colors.green,
-              //   child: Center(
-              //     child: Text('Hijau')
-              //   ),
-              // )
-          ),
-        ),
+        body: ListView.builder(
+          itemCount: 3, //batas / berapa banyak
+          itemBuilder: (context, index) => KotakWarna(
+            nama: 'Kotak ke - ${index+1}',
+            warna: Color.fromARGB(
+              255, 
+              150 + Random().nextInt(200), 
+              150 + Random().nextInt(200), 
+              150 + Random().nextInt(200), 
+            )
+          )
+        )
       ),
     );
   }
